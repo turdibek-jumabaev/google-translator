0f8d5cc3f0d111253d78ebff0067cc25c3ae2a3b
Turdibek <turdibekdev05@gmail.com>
2022-04-16 23:06:38 +0500

    update

--- google_trans/__main__.py
+++ google_trans/__main__.py
@@ -4,12 +4,29 @@ import requests
 class Translator:
 
     def __init__(self):
-        self.url = 'https://translate.google.com/translate_a/single?client=gtx&sl={}&tl={}&dt=t&q={}'
+        self.url = 'https://translate.google.com/'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
         }
 
-    def translate(self, text, dest='en', src='auto'):
-        response = requests.get(self.url.format(
-            src, dest, text), headers=self.headers)
-        return response.json()[0][0][0]
+    def translate(self, text: str, source_lang='auto', target_lang='uz'):
+        """
+        Translate text from source language to target language
+        :param text: text to translate
+        :param source_lang: source language
+        :param target_lang: target language
+        :return: translated text
+        """
+        params = {
+            'hl': 'en',
+            'ie': 'UTF-8',
+            'text': text,
+            'langpair': f'{source_lang}|{target_lang}'
+        }
+
+        if source_lang == 'auto':
+            del params['langpair']
+            return requests.get(
+                self.url + f"translate_a/single?client=gtx&sl=auto&tl={target_lang}&dt=t&q={text}", params=params, headers=self.headers).json()[0][0][0]
+        else:
+            return requests.get(url=self.url + f"translate_a/single?client=gtx&sl={source_lang}&tl={target_lang}&dt=t&q={text}", params=params, headers=self.headers).json()[0][0][0]
